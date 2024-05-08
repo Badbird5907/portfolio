6eeafbfcee8960bb56f2a5fc1ae6b50295b1eb3c
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-05-08 00:44:45 -0400

    fix version

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,11 @@ import Version from "@/components/version";
 
 const Footer = () => {
   return (
-    <footer className={"text-black-400 dark:text-gray-300 text-center"}>
+    <footer
+      className={
+        "fixed bottom-0 left-0 p-2 text-xs text-gray-500 dark:text-gray-400"
+      }
+    >
       <Version />
     </footer>
   );
--- src/components/version.tsx
+++ src/components/version.tsx
@@ -4,15 +4,13 @@ import Link from "next/link";
 const Version = async () => {
   const version = await getVersionString();
   return (
-    <div className={"flex flex-col items-center justify-center"}>
-      <Link
-        href={version?.url || "#"}
-        className={"text-gray-500 pb-4"}
-        target={"_blank"}
-      >
-        {version.version || "Unknown"}
-      </Link>
-    </div>
+    <Link
+      href={version?.url || "#"}
+      className={"text-gray-500"}
+      target={"_blank"}
+    >
+      {version.version || "Unknown"}
+    </Link>
   );
 };
 
