19532b605da2dd785fe86f61861e7456794ba400
Evan <50347938+Badbird5907@users.noreply.github.com>
2025-01-26 17:06:36 -0500

    Update navbar.tsx

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 
 const nav = [
   { href: "/", label: "Home" },
-  { href: "/blog", label: "Blog" },
+  // { href: "/blog", label: "Blog" },
 ];
 
 const Navbar = () => {
